c209b3cb55ddee264eef99acf8688d1fc84601ee
rabieLakhdar <rlakhdar@maltem.com>
2020-12-21 13:56:40 +0100

    endpoints to start

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import * as express from 'express'
 
 const prisma = new PrismaClient()
+const app = express()
 
-async function main() {
-    const newPost = await prisma.post.create({
-        data: {
-            title: 'rabiel post',
-            content: ' rabiel fullstack js developer'
-        },
-      })
-      console.log('Created new post: ', newPost)
-    
-      const allPosts = await prisma.post.findMany()
-      console.log('All posts: ')
-      console.dir(allPosts, { depth: null })
-}
+app.use(express.json())
 
-main()
-  .catch((e) => console.error(e))
-  .finally(async () => await prisma.$disconnect())
+app.get('/posts', async (req, res) => {
+  const posts = await prisma.post.findMany()
+  res.json(posts)
+})
+
+app.get(`/post/:id`, async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  })
+  res.json(post)
+})
+
+app.listen(3000, () =>
+  console.log('REST API server ready at: http://localhost:3000'),
+)
